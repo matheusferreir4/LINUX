56c2f887d7f06e198cee42c60b9a5c104f850830
Matheus Ferreira <161968118+matheusferreir4@users.noreply.github.com>
2024-03-21 19:48:09 -0300

    Update atividade3.sh

--- aula 3/atividade3.sh
+++ aula 3/atividade3.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
 cd ~
+
 ls
+
 mkdir teste1
+
 mkdir teste2 teste3
+
 cd teste1
+
 ls
+
 cd .. && cd teste2
+
 ls
+
 echo -e "Linha1\nLinha2\nLinha3\n\nLinha4\n\n\n\nLinha5\n\n\n\n\n\n\nLinha6" > arquivo1.txt
+
 cat arquivo1.txt
+
 cat -n arquivo1.txt
+
 cat -s arquivo1.txt
 
